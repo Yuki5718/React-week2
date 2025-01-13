57bb94dc4a2a04c5feb73f8b562e71cb20022fcc
yuki5718 <hayate8575@gmail.com>
2025-01-14 00:56:37 +0800

    修正部屬時的產品路徑

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 產品路徑、開發中路徑
+  base: process.env.NODE_ENV === 'production' ? '/React-week2/' : '/',
   plugins: [react()],
 })
